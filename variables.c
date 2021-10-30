650a2e6fd1a201b41e33ba76911b0dbc45776e7e
Satya-manohar <93309674+Satya-manohar@users.noreply.github.com>
2021-10-30 12:06:47 +0530

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	// intialization
+	char initial;
+	int age;
+	float cgpa;
+	
+	//variable declaration
+	initial= 'K';
+	age = 19;
+	cgpa = 9.0;
+	
+	
+	//output
+	printf("INITIAL of the student is %c\n", initial );
+	printf("age of the student is %d\n", age);
+	printf("CGPA of the student is %f", cgpa);
+}
+
---
+++ variables2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+
+{	//variables
+	float num1 = 2.0, num2 = 3.0, sum = num1 + num2;
+	
+	printf("SUM OF %f AND %f IS  %f",num1, num2, sum);
+}
