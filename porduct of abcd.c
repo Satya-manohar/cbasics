0304f9ef95cfbb56454ea4d4cd6103b9f283cc49
Satya-manohar <93309674+Satya-manohar@users.noreply.github.com>
2021-11-10 22:07:01 -0800

    Add files via upload

---
+++ porduct of abcd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, c, d;
+	printf("enter 1st number");
+	scanf("%d", &a);
+	printf("enter 2nd number");
+	scanf("%d", &b);
+	printf("enter 3rd number");
+	scanf("%d", &c);
+	printf("enter 4th number");
+	scanf("%d", &d);
+	printf("product of %d, %d, %d and %d is %d", a, b, c, d, a*b*c*d);
+}
