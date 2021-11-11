eba17e6680894879f2341617fbc1eb3130c846ab
Satya-manohar <93309674+Satya-manohar@users.noreply.github.com>
2021-11-10 22:33:12 -0800

    Delete square area & perimeter.c

--- square area & perimeter.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int a,b,c,d;
-	printf("eneter all number to multiply");
-	scanf("%d%d%d%d",  &a, &b, &c, &d);
-	printf("product of %d, %d, %d and %d is %d", a, b, c, d, a*b*c*d);
-}
